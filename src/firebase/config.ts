512fab4dd3a55a7134ab13fc277322b2e3c8f458
puttuthakku500-alt <puttuthakku500@gmail.com>
2025-11-23 18:13:54 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,28 +3,18 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 //THIS IS THE MAIN DB
-/*const firebaseConfig = {
-  apiKey: "AIzaSyD57ySp5pnHPxX_3UaveoC62lZHFOUrT-Y",
-  authDomain: "bossinn-44651.firebaseapp.com",
-  projectId: "bossinn-44651",
-  storageBucket: "bossinn-44651.firebasestorage.app",
-  messagingSenderId: "510548886376",
-  appId: "1:510548886376:web:2a4d9f0e8724090cc7ccdd",
-  measurementId: "G-596DPL3V12"
-};*/
-
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBeiejGHWW0ZqQi4vzUGy-l547h18J-f0s",
+//   authDomain: "balaji-inn.firebaseapp.com",
+//   projectId: "balaji-inn",
+//   storageBucket: "balaji-inn.firebasestorage.app",
+//   messagingSenderId: "393579021304",
+//   appId: "1:393579021304:web:aa18acc562e1a8bcd24c0d",
+//   measurementId: "G-YFSTJ5YMWZ"
+// };
 
 //TESTING
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA-pRxNph8ECQEi2IzUWgPAq06fi-3wKK8",
-//   authDomain: "bossinn-7fbb9.firebaseapp.com",
-//   projectId: "bossinn-7fbb9",
-//   storageBucket: "bossinn-7fbb9.firebasestorage.app",
-//   messagingSenderId: "138358328390",
-//   appId: "1:138358328390:web:e04ab1e548d7c56e5fce47",
-//   measurementId: "G-8KRR5T7YJQ"
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyAIxrA5uo64FQOq_RP1GnG8sNhhXJBaX2E",
   authDomain: "boss-test-3d2f8.firebaseapp.com",
